e08356d161358b6e2cadf9c28cf8307b2fd1ce77
Hai <nguyenviet.hai1802@gmail.com>
2017-12-28 10:25:27 +0700

    Finish Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+  before_action :fetch_user, only: :create
+
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      check = params[:session][:remember_me] == Settings.remember_checked
+      check ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = t("flash.login_fail")
       render :new
@@ -13,7 +16,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def fetch_user
+    @user = User.find_by email: params[:session][:email].downcase
   end
 end
