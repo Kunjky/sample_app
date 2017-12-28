410022414cea56391aea2b8a82163f16afb8ee13
Hai <nguyenviet.hai1802@gmail.com>
2017-12-28 13:34:18 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+  before_action :fetch_user, except: %i(new create index)
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-    return if (@user = User.find_by(id: params[:id]))
-    flash[:danger] = t "flash.user_not_found", id: params[:id]
-    redirect_to root_path
-  end
+  def show; end
 
   def create
     @user = User.new user_params
@@ -20,10 +25,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "flash.profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t "flash.user_deleted"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def fetch_user
+    return if (@user = User.find_by(id: params[:id]))
+    flash[:danger] = t "flash.user_not_found", id: params[:id]
+    redirect_to root_path
   end
 end
